6080850d801afbf19a59c9c7a1c3396e05b446e0
Ifeanyi <ikalu13@gmail.com>
2014-08-04 11:34:04 -0400

    * Add assign admin ability via rails console

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,9 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.
+      require(:user).
+      permit(:email, :password)
   end
 end
 
