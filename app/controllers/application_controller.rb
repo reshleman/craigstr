73d65cfebc2bc89de7b873f7cecd93d8cc9e1df5
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-08-04 10:32:45 -0400

    Require login for all controller actions by default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include Monban::ControllerHelpers
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_action :require_login
+
   protect_from_forgery with: :exception
 end
