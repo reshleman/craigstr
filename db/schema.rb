618383e1d9269fab9d01087421edc6d5e8616838
Ifeanyi <ikalu13@gmail.com>
2014-08-04 13:53:53 -0400

    * Modify user-admin default value at database level

--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,11 @@ ActiveRecord::Schema.define(version: 20140804153123) do
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.boolean  "admin"
+    t.string   "email",                           null: false
+    t.string   "password_digest",                 null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
