0fccdb3c5c6172ab06113fdc5dec4492d769a9fe
Ifeanyi <ikalu13@gmail.com>
2014-08-04 11:50:00 -0400

    * Modify user/new to default admin - false

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,10 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.
+      require(:user).
+      permit(:email, :password).
+      merge(admin: :false)
   end
 end
 
