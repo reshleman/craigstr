32fba4cc6782e35c3d72b5da0f2f818be565336a
Ifeanyi <ikalu13@gmail.com>
2014-08-05 15:56:05 -0400

    * As an admin, I can create categories

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,14 @@ Rails.application.routes.draw do
     root "locations#index"
   end
 
-  resources :locations, only: [:index]
+  resources :locations, only: [:index, :show] do
+    resources :categories, only: [:index, :show]
+  end
+
 
   namespace :admin do
-    resources :locations, only: [:new, :create]
+    resources :locations, only: [:new, :create] do
+      resources :categories, only: [:new, :create]
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,27 +1,15 @@
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804174148) do
-
-  # These are extensions that must be enabled in order to support this database
+ActiveRecord::Schema.define(version: 20140805183913) do
   enable_extension "plpgsql"
-
+  create_table "categories", force: true do |t|
+    t.string  "name"
+    t.integer "location_id"
+  end
   create_table "locations", force: true do |t|
     t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
   create_table "users", force: true do |t|
     t.string   "email",                           null: false
     t.string   "password_digest",                 null: false
@@ -29,7 +17,5 @@ ActiveRecord::Schema.define(version: 20140804174148) do
     t.datetime "updated_at",                      null: false
     t.boolean  "admin",           default: false, null: false
   end
-
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-
 end
