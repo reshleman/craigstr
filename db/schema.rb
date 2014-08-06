4446f1f43f29a582d18ed9324aae6cbaaf01cf8e
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-08-06 15:07:02 -0400

    Create model and migration for spam flags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806144622) do
+ActiveRecord::Schema.define(version: 20140806190546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 20140806144622) do
   end
 
   add_index "posts", ["category_id"], name: "index_posts_on_category_id", using: :btree
+
+  create_table "spam_flags", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "spam_flags", ["post_id"], name: "index_spam_flags_on_post_id", using: :btree
+  add_index "spam_flags", ["user_id"], name: "index_spam_flags_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                           null: false
