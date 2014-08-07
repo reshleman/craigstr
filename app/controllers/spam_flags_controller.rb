dd5d7f45510a02a57c683393346e416de57e05de
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-08-07 10:01:59 -0400

    Update paths for nested spam flags route

--- app/controllers/spam_flags_controller.rb
+++ app/controllers/spam_flags_controller.rb
@@ -3,6 +3,6 @@ class SpamFlagsController < ApplicationController
     post = Post.find(params[:post_id])
     current_user.flag(post)
 
-    redirect_to [post.category.location, post.category, post]
+    redirect_to post
   end
 end
