9bf4923ade07f123fb1d7d912f78b0f6e14a5b8d
Ifeanyi <ikalu13@gmail.com>
2014-08-07 15:45:33 -0400

    fix p tag error

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @location = Location.find(params[:location_id])
     @category = Category.find(params[:id])
-    #@posts = @category.posts
     @posts = @category.posts.order(:title).post params[:post]
   end
 end
