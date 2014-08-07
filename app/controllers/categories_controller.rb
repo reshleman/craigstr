aade0cbacb09723eeed887aff26bf65ac3fa79a2
Ifeanyi <ikalu13@gmail.com>
2014-08-07 15:26:43 -0400

    A user can view posts in a paginated list
    * install Kaminari gem
    * run bundle and the kaminari generator
    * modify the generated config/initializers
    * modify post model to override default kaminari config
    * modify categories controller to implement pagination
    * modify category#show to implement pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
   def show
     @location = Location.find(params[:location_id])
     @category = Category.find(params[:id])
-    @posts = @category.posts
+    #@posts = @category.posts
+    @posts = @category.posts.order(:title).post params[:post]
   end
 end
